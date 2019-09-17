806e132ebb98e35ba6ec31183db0581c47941834
Abdou Sam <abdou.sam7@gmail.com>
2019-09-17 18:24:07 +0100

    UART and UDP Host (#4)
    * Ad TODO list for next step
    
    * Add Arguments for UDP
    
    * Impliment udp and uart mode as separate modes
    
    * Set Classes to interface communication streams:
    - Create abstract class for Datastream
    - Create UDP and UART Stream classes
    - Code not finished not tested yet
    
    * Make common connection stream for both uart & udp
    
    - One stream call to read and send
    - add a second version CRC table
    
    * Hotfix : Adapt ABC to be used with python2
    
    * Optimize code and add comment
    
    - remove unecessary while in udp read response
    - updated TODO list
    
    * Optimize send and read requests
    
    * HotFix CRC TABLE
    
    * Remove Unecessary Method udp_read and uart_read
    
    * Remove the TODO list in comment section
    
    * Update README
    
    * Cleanup code
    
    * Code optimization
    
    - Remove duplicate code and put it in parent class
    - Fix exception handleing, make it propagate
    - Add --crc option with default to table 1
    
    * Fix a print and update author
    
    * Minor comment fix
    
    * Cleanup code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AN1388-linux
 
-Implementation of Microchip's [AN1388][an1388] on Linux using UART.
+Implementation of Microchip's [AN1388][an1388] on Linux using UART and UDP.
 
 Copyright &copy; 2015-2016 Camil Staps. Licensed under GPL v3.0, see the LICENSE file for more details.
 
--- programmer.py
+++ programmer.py
@@ -1,31 +1,145 @@
 #!/usr/bin/env python
-"""Implementation of Microchip's AN1388 on Linux using UART"""
+"""Implementation of Microchip's AN1388 on Linux using UART/UDP"""
 
 from __future__ import print_function
 
 import sys
 
-import serial
 from argparse import ArgumentParser, RawTextHelpFormatter
 from binascii import hexlify, unhexlify
+from abc import ABCMeta, abstractmethod
 
-__author__ = "Camil Staps, V Govorovski"
+import socket
+import serial
+
+__author__ = "Camil Staps, V Govorovski, A Abdellah"
 __copyright__ = "Copyright 2015, Camil Staps"
 __credits__ = [
     "Camil Staps",
     "Ganapathi Ramachandra (Microchip Technology Inc.)",
-    "Vadim Govorovski (Interface Devices Ltd.)"]
+    "Vadim Govorovski (Interface Devices Ltd.)",
+    "Alaouchiche Abdellah"]
 __license__ = "GPL"
 __version__ = "0.3"
 __maintainer__ = "Camil Staps"
 __email__ = "info@camilstaps.nl"
 __status__ = "Development"
 
+# These tables are excatly the same, except the [-2] element. It depends on the
+# version of the bootloader library you are using on your PIC MCU.
 CRC_TABLE = [
-    0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
-    0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1c1, 0xf1ef]
+    [0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
+     0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1c1, 0xf1ef],
+    [0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7,
+     0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad, 0xe1ce, 0xf1ef]]
 
 DEBUG_LEVEL = 0
+CRC_INDEX = 0
+
+class DataStream:
+    """Abstract class for interfaces to the chip"""
+    __metaclass__ = ABCMeta
+
+    global DEBUG_LEVEL # pylint: disable=global-statement
+
+    def __init__(self):
+        pass
+
+    def read_response(self, command):
+        """Retrieve response from the chip"""
+        response = self.sub_read_response()
+
+        if DEBUG_LEVEL >= 2:
+            print('<', hexlify(response))
+
+        if response[0] != '\x01' or response[-1] != '\x04':
+            raise IOError('Invalid response from bootloader')
+
+        response = unescape(response[1:-1])
+
+        # Verify SOH, EOT and command fields
+        if response[0] != command:
+            raise IOError('Unexpected response type from bootloader')
+        if crc16(response[:-2]) != response[-2:]:
+            raise IOError('Invalid CRC from bootloader')
+
+        return response[1:-2]
+
+    def send_request(self, command):
+        """Build and send request"""
+        command = escape(command)
+        request = '\x01' + command + escape(crc16(command)) + '\x04'
+
+        self.sub_send_request(request)
+
+        if DEBUG_LEVEL >= 2:
+            print('>', hexlify(request))
+
+        return len(request)
+
+    @abstractmethod
+    def sub_read_response(self):
+        """Implementation-specific method to retrieve data from the chip"""
+        pass
+
+    @abstractmethod
+    def sub_send_request(self, request):
+        """Implementation-specific method to send data to the chip"""
+        pass
+
+
+class UDPStream(DataStream):
+    """UDP interface"""
+    def __init__(self, udp_addr, udp_port, timeout):
+        self.udp_addr = udp_addr
+        self.udp_port = udp_port
+        self.timeout = timeout
+        self.soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.soc.settimeout(self.timeout)
+
+        super(UDPStream, self).__init__()
+
+    def sub_read_response(self):
+        try:
+            response, _ = self.soc.recvfrom(1024)
+            return response
+        except Exception:
+            raise IOError('Bootloader response timed out')
+
+    def sub_send_request(self, request):
+        self.soc.sendto(request, (self.udp_addr, self.udp_port))
+
+
+class UARTStream(DataStream):
+    """UART interface"""
+    def __init__(self, uart_port, uart_baud, timeout):
+        self.uart_port = uart_port
+        self.uart_baud = uart_baud
+        self.timeout = timeout
+        self.ser = serial.Serial(self.uart_port,
+                                 self.uart_baud,
+                                 timeout=self.timeout)
+
+        super(UARTStream, self).__init__()
+
+    def sub_read_response(self):
+        response = ''
+
+        while len(response) < 4 \
+              or response[-1] != '\x04' or response[-2] == '\x10':
+
+            byte = self.ser.read(1)
+
+            if len(byte) == 0: # pylint: disable=len-as-condition
+                raise IOError('Bootloader response timed out')
+            if byte == '\x01' or len(response) > 0: # pylint: disable=len-as-condition
+                response += byte
+
+        return response
+
+    def sub_send_request(self, request):
+        self.ser.write(request)
+
 
 def crc16(data):
     """Calculate the CRC-16 for a string"""
@@ -33,15 +147,37 @@ def crc16(data):
     crc = 0
     for byte in data:
         i = (crc >> 12) ^ (ord(byte) >> 4)
-        crc = CRC_TABLE[i & 0x0f] ^ (crc << 4)
+        crc = CRC_TABLE[CRC_INDEX][i & 0x0f] ^ (crc << 4)
         i = (crc >> 12) ^ (ord(byte) >> 0)
-        crc = CRC_TABLE[i & 0x0f] ^ (crc << 4)
+        crc = CRC_TABLE[CRC_INDEX][i & 0x0f] ^ (crc << 4)
 
     return chr(crc & 0xff) + chr((crc >> 8) & 0xff)
 
 def parse_args():
     """Parse command line arguments"""
     pars = ArgumentParser(formatter_class=RawTextHelpFormatter)
+
+    pars.add_argument(
+        '-i', '--interface',
+        help='Choose bootloader communication interface',
+        choices=['uart', 'udp'],
+        required=True)
+
+    pars.add_argument(
+        '-a', '--udp-addr',
+        help='IP Address for UDP')
+    pars.add_argument(
+        '-n', '--udp-port',
+        help='UDP port number',
+        type=int, default=6234)
+
+    pars.add_argument(
+        '-p', '--port',
+        help='Serial port to use')
+    pars.add_argument(
+        '-b', '--baud',
+        help='Baudrate to the bootloader',
+        type=int, default=115200)
 
     pars.add_argument(
         '-u', '--upload',
@@ -62,25 +198,15 @@ def parse_args():
         '-r', '--run',
         help='Run after upload',
         action='store_true')
-
     pars.add_argument(
         '-v', '--version',
         help='Read bootloader version',
         action='store_true')
 
     pars.add_argument(
-        '-p', '--port',
-        help='Serial port to use',
-        required=True)
-    pars.add_argument(
-        '-b', '--baud',
-        help='Baudrate to the bootloader',
-        type=int, default=115200)
-    pars.add_argument(
         '-t', '--timeout',
         help='Timeout in seconds',
         type=float, default=1.0)
-
     pars.add_argument(
         '-D', '--debug',
         help='Debug level',
@@ -90,6 +216,12 @@ def parse_args():
         '--my-version',
         action='version',
         version='%(prog)s ' + __version__)
+
+    pars.add_argument(
+        '--crc',
+        help='CRC table',
+        choices=['0', '1'],
+        default=1)
 
     return pars.parse_args()
 
@@ -114,45 +246,7 @@ def unescape(data):
             record += byte
     return record
 
-def send_request(port, command):
-    """Send a command over a serial port"""
-    command = escape(command)
-
-    # Build and send request
-    request = '\x01' + command + escape(crc16(command)) + '\x04'
-    port.write(request)
-    if DEBUG_LEVEL >= 2:
-        print('>', hexlify(request))
-    return len(request)
-
-def read_response(port, command):
-    """Read the response from the serial port"""
-    response = ''
-    while len(response) < 4 \
-          or response[-1] != '\x04' or response[-2] == '\x10':
-        byte = port.read(1)
-        if len(byte) == 0:
-            raise IOError('Bootloader response timed out')
-        if byte == '\x01' or len(response) > 0:
-            response += byte
-
-    if DEBUG_LEVEL >= 2:
-        print('<', hexlify(response))
-
-    if response[0] != '\x01' or response[-1] != '\x04':
-        raise IOError('Invalid response from bootloader')
-
-    response = unescape(response[1:-1])
-
-    # Verify SOH, EOT and command fields
-    if response[0] != command:
-        raise IOError('Unexpected response type from bootloader')
-    if crc16(response[:-2]) != response[-2:]:
-        raise IOError('Invalid CRC from bootloader')
-
-    return response[1:-2]
-
-def upload(port, filename):
+def upload(conn_stream, filename):
     """Upload a hexfile"""
     txcount, rxcount, txsize, rxsize = 0, 0, 0, 0
     with open(filename) as hexfile:
@@ -167,8 +261,8 @@ def upload(port, filename):
                 sys.stdout.flush()
             # Convert from ASCII to hexdec
             data = unhexlify(line[1:-1])
-            txsize += send_request(port, '\x03' + data)
-            response = read_response(port, '\x03')
+            txsize += conn_stream.send_request('\x03' + data)
+            response = conn_stream.read_response('\x03')
             rxsize += len(response) + 4
             txcount += 1
             rxcount += 1
@@ -178,25 +272,42 @@ def upload(port, filename):
 def main():
     """Main programmer function"""
     global DEBUG_LEVEL # pylint: disable=global-statement
+    global CRC_INDEX # pylint: disable=global-statement
 
     args = parse_args()
+
     DEBUG_LEVEL = args.debug
-    ser = serial.Serial(args.port, args.baud, timeout=args.timeout)
+    CRC_INDEX = int(args.crc)
+
+    if args.interface == 'uart':
+        if args.port is None:
+            raise ValueError("--port is required with the UART interface")
+
+        print("Connecting to %s at baudrate %d.." % (args.port, args.baud))
+
+        conn_stream = UARTStream(args.port, args.baud, args.timeout)
+    elif args.interface == 'udp':
+        if args.udp_addr is None:
+            raise ValueError("--udp-addr is required with the UDP interface")
+
+        print("Connecting to %s:%d.." % (args.udp_addr, args.udp_port))
+
+        conn_stream = UDPStream(args.udp_addr, args.udp_port, args.timeout)
 
     if args.version:
         print('Querying..')
-        send_request(ser, '\x01')
-        version = read_response(ser, '\x01')
+        conn_stream.send_request('\x01')
+        version = conn_stream.read_response('\x01')
         print('Bootloader version: ' + hexlify(version))
 
     if args.erase:
         print('Erasing..')
-        send_request(ser, '\x02')
-        read_response(ser, '\x02')
+        conn_stream.send_request('\x02')
+        conn_stream.read_response('\x02')
 
     if args.upload != None:
         print('Uploading..')
-        upstats = upload(ser, args.upload)
+        upstats = upload(conn_stream, args.upload)
         print(
             'Transmitted: %d packets (%d bytes), '\
             'Received: %d packets (%d bytes)' % upstats)
@@ -205,14 +316,14 @@ def main():
         print('Verifying..')
         addr, size = args.check.split(':')
         addr, size = addr.zfill(8), size.zfill(8)
-        send_request(
-            ser, '\x04' + unhexlify(addr)[::-1] + unhexlify(size)[::-1])
-        checksum = read_response(ser, '\04')
+        conn_stream.send_request(
+            '\x04' + unhexlify(addr)[::-1] + unhexlify(size)[::-1])
+        checksum = conn_stream.read_response('\04')
         print('CRC @%s[%s]: %s' % (addr, size, hexlify(checksum)))
 
     if args.run:
         print('Running application..')
-        send_request(ser, '\x05')
+        conn_stream.send_request('\x05')
 
     print('Done.')
 
